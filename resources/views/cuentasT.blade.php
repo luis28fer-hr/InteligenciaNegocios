e10d2347f11d55d0e5339d1bb3cf4db9c1efecb9
arwST <105440994+zeroRW@users.noreply.github.com>
2023-06-04 21:22:28 -0600

    Entrega para tomorrow
    Segundo boceto de las cuentas T

--- resources/views/cuentasT.blade.php
+++ resources/views/cuentasT.blade.php
@@ -8,67 +8,111 @@
         <h2>Cuentas T</h2>
         <p>Representación visual que permite un seguimiento claro de los movimientos contables.</p>
 
-        <br>
+        
+        
+        <button id="agregar-card">Agregar Card</button>
 
-        <style>
-            .card {
-                width: 400px;
-                background-color: lightblue;
-                margin: 10px;
-                padding: 10px;
-                float: left;
-            }
+        <div id="card-container">
+            <!-- Aquí se agregarán los cards dinámicamente -->
+        </div>
+    </div>
 
-            .card input[type="text"] {
-                width: 100%;
-                margin-bottom: 10px;
-            }
+    <style>
+        .card {
+            margin-bottom: 20px;
+            padding: 10px;
+            border: 1px solid #ccc;
+        }
+        .card input {
+            margin-bottom: 10px;
+        }
+        .card table {
+            margin-bottom: 10px;
+        }
+        .card table th {
+            text-align: center;
+        }
+        .card table td {
+            padding: 5px;
+        }
+    </style>
 
-            .card table {
-                width: 100%;
-                border-collapse: collapse;
-            }
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            var cardIndex = 1;
 
-            .card th, .card td {
-                padding: 5px;
-                border: 1px solid #000;
-            }
+            // Agregar un nuevo card al hacer clic en el botón
+            $('#agregar-card').on('click', function() {
+                var cardId = 'card-' + cardIndex;
+                var cardHtml = `
+                    <div id="${cardId}" class="card">
+                        <input type="text" placeholder="Título">
+                        <button class="guardar-titulo">Guardar</button>
+                        <table>
+                            <tr>
+                                <th>Lo que tengo</th>
+                                <th>Lo que no tengo</th>
+                            </tr>
+                            <tr>
+                                <td contenteditable="true" class="lo-que-tengo">0</td>
+                                <td contenteditable="true" class="lo-que-no-tengo">0</td>
+                            </tr>
+                        </table>
+                        <button class="agregar-fila">Agregar Fila</button>
+                        <input type="text" value="0" disabled class="suma-lo-que-tengo">
+                        <input type="text" value="0" disabled class="suma-lo-que-no-tengo">
+                        <input type="text" value="0" disabled class="resultado-resta">
+                        <button class="eliminar-card">Eliminar Card</button>
+                    </div>
+                `;
+                $('#card-container').append(cardHtml);
+                cardIndex++;
 
-            .card .add-row-btn {
-                background-color: #4CAF50;
-                color: white;
-                padding: 5px 10px;
-                border: none;
-                cursor: pointer;
-            }
+                // Funcionalidad para guardar el título
+                $('#' + cardId + ' .guardar-titulo').on('click', function() {
+                    var titulo = $('#' + cardId + ' input[type="text"]').val();
+                    $('#' + cardId + ' h3').text(titulo);
+                });
 
-            .card .add-row-btn:hover {
-                opacity: 0.8;
-            }
+                // Funcionalidad para agregar filas a la tabla
+                $('#' + cardId + ' .agregar-fila').on('click', function() {
+                    var filaHtml = `
+                        <tr>
+                            <td contenteditable="true" class="lo-que-tengo">0</td>
+                            <td contenteditable="true" class="lo-que-no-tengo">0</td>
+                        </tr>
+                    `;
+                    $('#' + cardId + ' table').append(filaHtml);
+                });
 
-        </style>
+                // Actualizar la suma de "Lo que tengo" y "Lo que no tengo" al cambiar los valores en la tabla
+                $('#' + cardId + ' table').on('input', 'td.lo-que-tengo, td.lo-que-no-tengo', function() {
+                    var sumTengo = 0;
+                    var sumNoTengo = 0;
 
-        <script>
-            $(document).ready(function() {
-                var cardCount = 0;
-
-                $('#add-card-btn').click(function() {
-                    cardCount++;
-                    var newCard = $('<div class="card"><input type="text" name="card_title" placeholder="Enter title"><table><thead><tr><th>Lo que tengo</th><th>Lo que no tengo</th></tr></thead><tbody></tbody></table><button class="add-row-btn">Add Row</button></div>');
-                    $('#card-container').append(newCard);
-
-                    newCard.find('.add-row-btn').click(function() {
-                        var newRow = $('<tr><td><input type="text" name="tengo[]" placeholder="Enter content"></td><td><input type="text" name="no_tengo[]" placeholder="Enter content"></td></tr>');
-                        newCard.find('tbody').append(newRow);
+                    $('#' + cardId + ' table td.lo-que-tengo').each(function() {
+                        sumTengo += parseInt($(this).text()) || 0;
                     });
+
+                    $('#' + cardId + ' table td.lo-que-no-tengo').each(function() {
+                        sumNoTengo += parseInt($(this).text()) || 0;
+                    });
+
+                    $('#' + cardId + ' .suma-lo-que-tengo').val(sumTengo);
+                    $('#' + cardId + ' .suma-lo-que-no-tengo').val(sumNoTengo);
+                    $('#' + cardId + ' .resultado-resta').val(sumTengo - sumNoTengo);
+                });
+
+                // Funcionalidad para eliminar el card
+                $('#' + cardId + ' .eliminar-card').on('click', function() {
+                    $('#' + cardId).remove();
                 });
             });
-        </script>
-
-        <button id="add-card-btn">Add Card</button>
-        <div id="card-container"></div>
+        });
+    </script>
             
-        </div>
+       
 
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\TarjetaController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', [LoginController::class, 'index'])->name('login');
 Route::get('login', [LoginController::class, 'login'])->name('login.session');
